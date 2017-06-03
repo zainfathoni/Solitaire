d1c1980c024c1b80e8b2b18c6dba0f327c4f031a
Zain Fathoni <zain.fathoni@gmail.com>
2017-06-03 11:04:47 +0700

    step: Building The Foundation

--- src/GameModel.kt
+++ src/GameModel.kt
@@ -1,5 +1,10 @@
 class GameModel {
     val deck = Deck()
     val wastePile: MutableList<Card> = mutableListOf()
-
+    val foundationPile = arrayOf(
+        FoundationPile("Clubs"),
+        FoundationPile("Diamonds"),
+        FoundationPile("Hearts"),
+        FoundationPile("Spades")
+    )
 }
