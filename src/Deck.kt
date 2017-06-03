66fba3912dd4db1950a907f4409854b97e5a1ded
Zain Fathoni <zain.fathoni@gmail.com>
2017-06-03 08:35:06 +0700

    step: Using the Deck

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,5 +1,15 @@
+import java.util.*
+
 class Deck {
     val cards = Array(52, { Card(it % 13, getSuit(it)) })
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck = cards.toMutableList()
+        Collections.shuffle(cardsInDeck)
+    }
 
     private fun  getSuit(i: Int) = when(i / 13) {
         0 -> "Clubs"
