18b581b1e6ff3811fc70989307b9fed1757c9bca
Zain Fathoni <zain.fathoni@gmail.com>
2017-06-03 11:28:39 +0700

    step: Creating the Tableau

--- src/Card.kt
+++ src/Card.kt
@@ -2,5 +2,7 @@ val clubs = "Clubs"
 val diamonds= "Diamonds"
 val hearts = "Hearts"
 val spades = "Spades"
+var redSuits = arrayOf(diamonds, hearts)
+var blackSuits = arrayOf(clubs, spades)
 
-class Card(val value: Int, val suit: String, val faceUp: Boolean = false)
+class Card(val value: Int, val suit: String, var faceUp: Boolean = false)
